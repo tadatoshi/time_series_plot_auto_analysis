5f846ad892d6d4598fd7c6f6abe518089baa91ef
tadatoshi <tadatoshi@gmail.com>
2022-06-22 16:05:15 -0700

    Added the function to plot variogram and save it to file.

---
+++ time_series_plot_auto_analysis/variogram/plotting.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+
+def plot_and_save(data_frame: pd.DataFrame, file_path, xlabel='Lag k', ylabel='Gk', x_max=None, y_max=None):
+
+    if x_max is None:
+        x_max = len(data_frame)
+    if y_max is None:
+        y_max = np.ceil(data_frame.max().values[0])
+
+    data_frame.plot(marker='o', xlabel=xlabel, ylabel=ylabel,
+                    xlim=([1, x_max]), ylim=([1, y_max]),
+                    xticks=np.arange(1, x_max+1), yticks=np.arange(1, y_max+1, 0.5)
+                    ).get_figure().savefig(file_path)
