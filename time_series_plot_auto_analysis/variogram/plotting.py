562a912513787d1045ce81eeb515774f99ebea8d
tadatoshi <tadatoshi@gmail.com>
2022-06-22 16:15:46 -0700

    Variogram plotting: Removed legend.

--- time_series_plot_auto_analysis/variogram/plotting.py
+++ time_series_plot_auto_analysis/variogram/plotting.py
@@ -9,7 +9,7 @@ def plot_and_save(data_frame: pd.DataFrame, file_path, xlabel='Lag k', ylabel='G
     if y_max is None:
         y_max = np.ceil(data_frame.max().values[0])
 
-    data_frame.plot(marker='o', xlabel=xlabel, ylabel=ylabel,
+    data_frame.plot(marker='o', xlabel=xlabel, ylabel=ylabel, legend=None,
                     xlim=([1, x_max]), ylim=([1, y_max]),
                     xticks=np.arange(1, x_max+1), yticks=np.arange(1, y_max+1, 0.5)
                     ).get_figure().savefig(file_path)
