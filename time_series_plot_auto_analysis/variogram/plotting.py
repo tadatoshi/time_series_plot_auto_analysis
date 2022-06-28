9b9fc0a11683793c17753323c987224b5a808fe5
tadatoshi <tadatoshi@gmail.com>
2022-06-28 16:57:56 -0700

    time_series_plot_auto_analysis.variogram.plotting.plot_and_save function: Made it not to put marker by default and added a parameter to specify to put a marker.

--- time_series_plot_auto_analysis/variogram/plotting.py
+++ time_series_plot_auto_analysis/variogram/plotting.py
@@ -4,17 +4,20 @@ import matplotlib.pyplot as plt
 
 
 def plot_and_save(data_frame: pd.DataFrame, file_path, xlabel='Lag k', ylabel='Gk', x_max=None, y_max=None,
-                  axis_off=False, save_without_displaying_plot=False):
+                  marker_on=False, axis_off=False, save_without_displaying_plot=False):
 
     if x_max is None:
         x_max = len(data_frame)
     if y_max is None:
         y_max = np.ceil(data_frame.max().values[0])
 
-    fig = data_frame.plot(marker='o', xlabel=xlabel, ylabel=ylabel, legend=None,
-                          xlim=([1, x_max]), ylim=([1, y_max]),
-                          xticks=np.arange(1, x_max+1), yticks=np.arange(1, y_max+1, 0.5)
-                         ).get_figure()
+    arguments = {'xlabel':xlabel, 'ylabel':ylabel, 'legend':None,
+                 'xlim':([1, x_max]), 'ylim':([1, y_max]),
+                 'xticks':np.arange(1, x_max+1), 'yticks':np.arange(1, y_max+1, 0.5)}
+    if marker_on:
+        arguments['marker'] = 'o'
+
+    fig = data_frame.plot(**arguments).get_figure()
     if axis_off:
         plt.axis('off')
     if save_without_displaying_plot:
