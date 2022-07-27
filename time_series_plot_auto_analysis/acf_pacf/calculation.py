1d31eab94f39773e2674e88dffa93a99b719ec65
tadatoshi <tadatoshi@gmail.com>
2022-07-26 17:48:55 -0700

    Modified the code to generate ACF and PACF plots for nonstationary time series with trend component, in order to make the intension clearer.

--- time_series_plot_auto_analysis/acf_pacf/calculation.py
+++ time_series_plot_auto_analysis/acf_pacf/calculation.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 
 def add_syntheric_trend(generated_arma_sample_df: pd.DataFrame, trend_span_ratio=4):
-    max = generated_arma_sample_df.max()
-    min = generated_arma_sample_df.min()
+    max = generated_arma_sample_df.max().values[0]
+    min = generated_arma_sample_df.min().values[0]
     trend_span = (max - min) * trend_span_ratio
     trend = np.linspace(0, trend_span, len(generated_arma_sample_df))
-    return generated_arma_sample_df + trend
+    trend_df = pd.DataFrame(trend)
+    return generated_arma_sample_df + trend_df
